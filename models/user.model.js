baac4f291dd80cb4b2dd6b6389937cd32eb68529
Jenish Shah <jenishshah1604@gmail.com>
2022-07-12 21:19:48 +0530

    Database updated

--- models/user.model.js
+++ models/user.model.js
@@ -13,6 +13,16 @@ const userSchema = new mongoose.Schema({
         required: true
     },
 
+    email: {
+
+        type: String,
+        required: true
+    },
+    password:{
+        type: String,
+        required:true
+    },
+
     contactNumber:{
 
         type: String,
@@ -20,14 +30,7 @@ const userSchema = new mongoose.Schema({
 
     },
 
-    email: {
-
-        type: String,
-        required: true
-    },
-
-
-    addressLine: {
+    address: {
 
         type: String,
         required: true
